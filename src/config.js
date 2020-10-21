8f1cd288f7e42faa836182001ed35f1ff21408c5
oli m-c <olster98@gmail.com>
2020-10-21 14:43:05 +0100

    Update social media links

--- src/config.js
+++ src/config.js
@@ -93,13 +93,15 @@ const canonicalRootURL = process.env.REACT_APP_CANONICAL_ROOT_URL;
 const siteTitle = 'Saunatime';
 
 // Twitter handle is needed in meta tags (twitter:site). Start it with '@' character
-const siteTwitterHandle = '@sharetribe';
+const siteTwitterHandle = '@AllotMe_UK';
 
 // Instagram page is used in SEO schema (http://schema.org/Organization)
-const siteInstagramPage = null;
+const siteInstagramPage = 'https://www.instagram.com/allotme';
 
 // Facebook page is used in SEO schema (http://schema.org/Organization)
-const siteFacebookPage = 'https://www.facebook.com/Sharetribe/';
+const siteFacebookPage = 'https://www.facebook.com/allotme/';
+
+const siteLinkedInPage = 'https://www.bbc.co.uk'
 
 // Facebook counts shares with app or page associated by this id
 // Currently it is unset, but you can read more about fb:app_id from
